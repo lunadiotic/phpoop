bd3abd21158b108951f21230392c61dbf43ad62a
IDStack <idstack@outlook.co.id>
2020-04-21 10:33:59 +0700

    autoload method

---
+++ chapter5/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+function myAutoload($class)
+{
+    if (preg_match('/\A\w+\Z/', $class)) {
+        include 'classes/' . $class . ".php";
+    }
+}
+
+spl_autoload_register('myAutoload');
+
+// $student = new Student();
+// echo $student->name;
+
+$cat = new Cat();
+echo $cat->name;
